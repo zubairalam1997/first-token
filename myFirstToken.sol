5bf2ffb58f4b3399d8d8337e973e850d3af9c61b
Mohd Zubair Alam Ansari <106484066+zubairalam1997@users.noreply.github.com>
2023-08-14 21:19:23 +0530

    Update myFirstToken.sol

--- myFirstToken.sol
+++ myFirstToken.sol
@@ -1,23 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+/*
+       REQUIREMENTS
+    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
+    2. Your contract will have a mapping of addresses to balances (address => uint)
+    3. You will have a mint function that takes two parameters: an address and a value. 
+       The function then increases the total supply by that number and increases the balance 
+       of the “sender” address by that amount
+    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
+       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
+       and from the balance of the “sender”.
+    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
+       to the amount that is supposed to be burned.
+*/
+
 contract myFirstToken{
     string public tokenName = "ZUBAIR";
     string public tokenAbbrv = "Z";
-    uint public totalSupply = 0;
+    uint public tokenQuant = 0;
 
     mapping(address => uint)public balance;
 
-    function mint (address ownerAddress , uint tokenValue ) public {
-            totalSupply += tokenValue ;
-            balance[ownerAddress] += tokenValue;
+    function mint (address ownerAddress , uint numOfToken ) public {
+            tokenQuant += numOfToken ;
+            balance[ownerAddress] += numOfToken;
            
     }
 
-    function burn (address ownerAddress , uint tokenValue) public   {
-        if(balance[ownerAddress] >= tokenValue){
-        totalSupply -= tokenValue;
-        balance[ownerAddress] -= tokenValue;
+    function burn (address ownerAddress , uint numOfToken) public   {
+        if(balance[ownerAddress] >= numOfToken){
+        tokenQuant -= numOfToken;
+        balance[ownerAddress] -= numOfToken;
         
         }
     }
